7cdc343d9d4c0f4aa9fc80b0de971aadc18d4785
svilenstoicheff <sstoicheff@expedia.com>
2018-05-11 12:09:23 -0500

    adding grid payout example

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home page. */ 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 router.get('/template', function(req, res, next) {
   res.render('template', { title: 'Flex-box template' });
+});
+router.get('/grid', function(req, res, next) {
+  res.render('grid', { title: 'Grid' });
 });
 
 module.exports = router;
